e91c0d9eeb09d5e9642d4cb07e27cba172b7ec45
Erik Noorland <noorlandjr_e@hotmail.com>
2023-12-31 15:04:56 +0100

    Update code example to import correct artefact

--- examples/node/app.js
+++ examples/node/app.js
@@ -1,4 +1,4 @@
-const nodebotUtils = require('../../dist/robotlib.cjs');
+const nodebotUtils = require('../../dist/nodebot-utils.cjs');
 
 console.log(nodebotUtils);
 console.log(nodebotUtils.math.deg2rad(180));
